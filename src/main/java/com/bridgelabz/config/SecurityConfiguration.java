2077d90063d3182d1f3c98ad0fcd529d56835ab3
shubham.chavan689977@gmail.com <shubham.chavan689977@gmail.com>
2020-01-02 19:08:27 +0530

    added notes and label apis

--- src/main/java/com/bridgelabz/config/SecurityConfiguration.java
+++ src/main/java/com/bridgelabz/config/SecurityConfiguration.java
@@ -51,6 +51,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		return  new BCryptPasswordEncoder();
 	}
 
+	
 	@Override
 	@Bean
 	public AuthenticationManager authenticationManagerBean() throws Exception {
